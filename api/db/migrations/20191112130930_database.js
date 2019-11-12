08e0c0345737794508e066ce84a51ef7a3f748a9
Mark Gowen <mark.gowen78@gmail.com>
2019-11-12 14:06:14 -0800

    created landowners seed data

---
+++ api/db/migrations/20191112130930_database.js
@@ -0,0 +1,83 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("landowners", tbl => {
+      tbl.increments("landowner_id");
+
+      tbl.string("avatar", 255);
+      tbl.string("email", 128).notNullable.unique;
+      tbl.string("username", 128).notNullable.unique;
+      tbl.string("password", 128);
+      tbl.string("first_name", 128).notNullable;
+      tbl.string("last_name", 128).notNullable;
+    })
+    .createTable("users", tbl => {
+      tbl.increments("user_id");
+
+      tbl.string("avatar", 255);
+      tbl.string("email", 128).notNullable.unique;
+      tbl.string("username", 128).notNullable.unique;
+      tbl.string("password", 128);
+      tbl.string("first_name", 128).notNullable;
+      tbl.string("last_name", 128).notNullable;
+    })
+    .createTable("amenities", tbl => {
+      tbl.increments("amenity_id");
+
+      tbl.string("amenity_name", 255).notNullable;
+      tbl.string("icon", 255).notNullable;
+    })
+    .createTable("properties", tbl => {
+      tbl.increments("property_id");
+
+      tbl
+        .integer("landowner_id")
+        .notNullable()
+        .unsigned()
+        .references("landowner_id")
+        .inTable("landowners")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.string("property_name", 255).notNullable;
+      tbl.string("address", 255).notNullable;
+      tbl.string("city", 255).notNullable;
+      tbl.string("state", 255).notNullable;
+      tbl.string("image", 255).notNullable;
+      tbl
+        .integer("amenity_id")
+        .notNullable()
+        .unsigned()
+        .references("amenity_id")
+        .inTable("amenities")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("reservations", tbl => {
+      tbl.increments("reservation_id");
+
+      tbl
+        .integer("property_id")
+        .unsigned()
+        .references("property_id")
+        .inTable("properties")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl.date("start_date");
+      tbl.date("end_date");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("reservations")
+    .dropTableIfExists("properties")
+    .dropTableIfExists("amenities")
+    .dropTableIfExists("users")
+    .dropTableIfExists("landowners");
+};
---
+++ api/db/seeds/01-landowners.js
@@ -0,0 +1,100 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("landowners")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("landowners").insert([
+        {
+          landowner_id: 1,
+          avatar: "https://unsplash.com/photos/kMJp7620W6U",
+          first_name: "Evan",
+          last_name: "Pena",
+          email: "evan_pena@example.com",
+          username: "yellowmouse215",
+          password: "pass123"
+        },
+        {
+          landowner_id: 2,
+          avatar: "https://unsplash.com/photos/i2hoD-C2RUA",
+          first_name: "Irma",
+          last_name: "Richards",
+          email: "irma_richards@example.com",
+          username: "ticklishostrich764",
+          password: "pass123"
+        },
+        {
+          landowner_id: 3,
+          avatar: "https://unsplash.com/photos/IfDfxUrJasI",
+          first_name: "Claire",
+          last_name: "Fisher",
+          email: "claire_fisher@example.com",
+          username: "tinyleopard720",
+          password: "pass123"
+        },
+        {
+          landowner_id: 4,
+          avatar: "https://unsplash.com/photos/iFgRcqHznqg",
+          first_name: "Eduardo",
+          last_name: "Henry",
+          email: "eduardo_henry@example.com",
+          username: "crazyfrog418",
+          password: "pass123"
+        },
+        {
+          landowner_id: 5,
+          avatar: "https://unsplash.com/photos/moDxmttUP00",
+          first_name: "Albert",
+          last_name: "Alexander",
+          email: "albert_alexander@example.com",
+          username: "greenkoala518",
+          password: "pass123"
+        },
+        {
+          landowner_id: 6,
+          avatar: "https://unsplash.com/photos/mgoxHRNoT00",
+          first_name: "Audrey",
+          last_name: "Robertson",
+          email: "audrey_robertson@example.com",
+          username: "beautifulbutterfly101",
+          password: "pass123"
+        },
+        {
+          landowner_id: 7,
+          avatar: "https://unsplash.com/photos/MP0IUfwrn0A",
+          first_name: "Bessie",
+          last_name: "Bell",
+          email: "bessie_bell@example.com",
+          username: "bessiebell",
+          password: "pass123"
+        },
+        {
+          landowner_id: 8,
+          avatar: "https://unsplash.com/photos/6BI7fVlw_BA",
+          first_name: "Shawn",
+          last_name: "Murphy",
+          email: "shawn_murphy@example.com",
+          username: "silverduck204",
+          password: "pass123"
+        },
+        {
+          landowner_id: 9,
+          avatar: "https://unsplash.com/photos/OhKElOkQ3RE",
+          first_name: "Zack",
+          last_name: "Miles",
+          email: "zack_miles",
+          username: "zackmiles",
+          password: "pass123"
+        },
+        {
+          landowner_id: 10,
+          avatar: "https://unsplash.com/photos/bqe0J0b26RQ",
+          first_name: "Scarlett",
+          last_name: "Mckinney",
+          email: "scarlett_mckinney@example.com",
+          username: "scarlettmckinney",
+          password: "pass123"
+        }
+      ]);
+    });
+};
